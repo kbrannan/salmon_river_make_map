70699ae9165eefa71f495ea23406ad86bf4dadba
kbrannan <brannan.kevin@deq.state.or.us>
2015-11-30 14:02:57 -0800

    still trying to get "GetMap" to return the map image i want. I am currently in Turket for some reason

--- get-state-boundary-extent.R
+++ get-state-boundary-extent.R
@@ -16,6 +16,37 @@ min(sp.state.bnd@lines[[1]]@Lines[[1]]@coords[,1])
 
 min(sp.state.bnd@lines[[1]]@Lines[[1]]@coords[,2])
 
+get.lats <- function(x) sp.state.bnd@lines[[x]]@Lines[[1]]@coords[,2]
+
+# min lat
+lat.min <- min(
+  do.call(rbind,lapply(X = 1:19, 
+                       FUN = function(x) 
+                         min(sp.state.bnd@lines[[x]]@Lines[[1]]@coords[,2]))))
+# max lat
+lat.max <- max(
+  do.call(rbind,lapply(X = 1:19, 
+                       FUN = function(x) 
+                         max(sp.state.bnd@lines[[x]]@Lines[[1]]@coords[,2]))))
+# min lon
+lon.min <- min(
+  do.call(rbind,lapply(X = 1:19, 
+                       FUN = function(x) 
+                         min(sp.state.bnd@lines[[x]]@Lines[[1]]@coords[,1]))))
+# max lon
+lon.max <- max(
+  do.call(rbind,lapply(X = 1:19, 
+                       FUN = function(x) 
+                         max(sp.state.bnd@lines[[x]]@Lines[[1]]@coords[,1]))))
+
+mt.bbox.state.bnd <- c(c(lat.min,lat.max)c(lon.min,lon.max))
+
+
+array(c(lat.min,lon.min,lat.max,lon.max), dim=c(2,2))
+
+
+get.lats(2)
+
 str(as.character(sp.state.bnd@proj4string))
 
 proj4string(sp.state.bnd)
@@ -30,10 +61,40 @@ google <- "+init=epsg:3857"
 
 junk.g <- spTransform(junk,CRS(google))
 
+sp.state.bnd.g <- spTransform(sp.state.bnd,CRS(google))
+
+str(sp.state.bnd.g)
+
+str(sp.state.bnd.g@bbox)
+
+range(sp.state.bnd.g@bbox[1,])
+range(sp.state.bnd.g@bbox[2,])
+
+bbox <- qbbox(lat=range(sp.state.bnd.g@bbox[1,]),
+              lon = range(sp.state.bnd.g@bbox[2,]))
+junk.png <- GetMap(center = c(lat = 122.6819, lon = -45.52),
+                   zoom = 5,
+                   destfile = "junk.png", format = "png")
+
+
+
+
+junk.g@bbox
+
 str(junk.g)
 
 junk.g@bbox[1,]
 
-junk.png <- GetMap.bbox(lonR=junk.g@bbox[1,],latR=junk.g@bbox[2,])
+range(junk.g@bbox[1,])
+
+bbox <- qbbox(lon=range(junk.g@bbox[2,]),lat=range(junk.g@bbox[1,]))
+
+rm(junk.png)
+
+junk.png <- GetMap(center = c(lon = mean(bbox$lonR), lat = mean(bbox$latR)),
+                   zoom = 5,
+                   destfile = "junk.png", format = "png")
 
 PlotOnStaticMap(junk.png)
+
+
